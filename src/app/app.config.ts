b457c833e5ce8f6ff3f2937211a17cc8737ae151
Çınar Özgürgil <cozgurgil@gmail.com>
2025-02-23 17:06:13 +0300

    Hydration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+  provideClientHydration(), // Bu Angular'ın google ile çalışıp sitelerin daha hızlı rander edilmesi için çalıştırılan bir fonksüyondur.
+
   provideZoneChangeDetection({ eventCoalescing: true }),
   provideRouter([
     {
