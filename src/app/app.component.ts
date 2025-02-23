fee548652e03085fee10686e93883fbb630ef826
Çınar Özgürgil <cozgurgil@gmail.com>
2025-02-23 16:45:58 +0300

    Signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ExampleComponent } from './example/example.component';
 
@@ -6,19 +6,45 @@ import { ExampleComponent } from './example/example.component';
   selector: 'app-root',
   imports: [ExampleComponent],
   template:`
-    <h1> State Management - Input / Output</h1>
+
+    <h1>{{ count() }}</h1>
+    <br>
+    <button (click)="increment()">+</button>
+    <button (click)="decrement()">-</button>
+    <hr>
+    <ul>
+      <li *ngFor="let r of results"></li>
+    </ul>
+
+    <!-- <h1> State Management - Input / Output</h1>
     <strong><p>Benim Adım {{name}}</p></strong>
     <hr>
     <app-example name = 'Örnek'></app-example>
-    <app-example [name] = 'name' (changeNameEvent)= "changeName($event)"></app-example> <!-- Buradan bir değer almak istersek o zaman köşeli parantez yaparız. -->
+    <app-example [name] = 'name' (changeNameEvent)= "changeName($event)"></app-example>--> <!-- Buradan bir değer almak istersek o zaman köşeli parantez yaparız. -->
  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  name: string = "Çınar";
 
-  changeName(event: string) {
-    this.name = event;
+  count = signal(0); // Biz burada signal değeri sayesinde count değeri değiştiği zaman gidip de bütün sayfayı yenileme sadece buradaki değeri update'le diyoruz
+  // update() değeri ise signal değeri ile kullanılır ve içerisine parantez açtıktan sonra buraya alınan değer için bir ad veririz ve sonucunda ne olucağını söyleriz.
+  results = signal<string[]>([]);
+
+  countPlus = computed(() => this.count() + 1);
+  countEffect = effect(() => console.log(this.count() + "değişti"))
+
+  increment() {
+
+    // Update yerine kullanılacak bilecek bir de Set metodu vardır fakat aynı mantıkta ve aynı işlem için kullanılır bir farkı yoktur.
+    // this.count.set(this.count() + 1);
+
+    this.count.update((oldValue) => oldValue + 1);
+    this.results.update((oldValue) => [...oldValue, 'increment']);
+  }
+
+  decrement() {
+    this.count.update((oldValue) => oldValue - 1);
+    this.results.update((oldValue) => [...oldValue, 'decrement']);
   }
 
 }
